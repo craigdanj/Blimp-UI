fe713fbf6ae8b31a50c08f7a48a95d8e99bcf085
craigdanj <craigdanj@gmail.com>
2020-12-22 23:33:27 +0530

    heaight corrected

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 
 export default function Home() {
   return (
-    <div class="h-full">
-      <div className="bg-blue-400 py-4 px-8">
+    <div class="flex flex-col h-full">
+      <div className="flex-grow-0 bg-blue-400 py-4 px-8">
         <h1 className="text-white font-bold text-lg">Blimp UI</h1>
       </div>
 
-      <div className="flex h-full">
-        <div className="bg-gray-700 text-white w-1/4 p-8">
+      <div className="flex flex-grow">
+        <div className="bg-gray-600 text-white w-1/4 p-8">
           <p>Test</p>
           <p>Test</p>
         </div>
