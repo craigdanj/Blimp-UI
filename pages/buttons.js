61001b07915d32382de2184c8eb384b5e7e99664
craigdanj <craigdanj@gmail.com>
2020-12-23 00:21:35 +0530

    buttons route added

--- pages/buttons.js
+++ pages/buttons.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div class="flex flex-col h-full">
+			{/* Use Layout for sidebar and header */}
       <div className="flex-grow-0 bg-blue-400 py-4 px-8">
         <h1 className="text-white font-bold text-lg">
           <Link href="/">
@@ -24,23 +25,8 @@ export default function Home() {
 
         <div className="flex-auto p-8">
 
-          <h1 className="mb-5 text-2xl font-bold text-gray-500">Alerts</h1>
+          <h1 className="mb-5 text-2xl font-bold text-gray-500">Buttons</h1>
           
-          <div className="bg-red-200 px-4 py-3 border border-red-300 rounded text-red-700">
-            This is a danger alert
-          </div>
-
-          <div className="bg-green-200 px-4 py-3 border border-green-300 rounded text-green-700 mt-5">
-            This is a success
-          </div>
-
-          <div className="bg-yellow-200 px-4 py-3 border border-yellow-300 rounded text-yellow-700 mt-5">
-            This is a warning
-          </div>
-
-          <div className="bg-blue-200 px-4 py-3 border border-blue-300 rounded text-blue-700 mt-5">
-            This is an info alert
-          </div>
         </div>
 
       </div>
